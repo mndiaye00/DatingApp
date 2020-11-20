d88ef8682c3261fc1acf6edee1d3e2fce8c3243e
mndiaye00 <msanes13@gmail.com>
2020-11-20 14:50:18 -0500

    Add routing

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -11,6 +11,10 @@ import { NavComponent } from './nav/nav.component';
 import { FormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   imports: [
@@ -25,7 +29,11 @@ import { RegisterComponent } from './register/register.component';
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    MemberListComponent,
+    MemberDetailComponent,
+    ListsComponent,
+    MessagesComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
